52b3c83a0c6074fdf3f1975f26d9e39154b03cf2
Corentin <Corentin@10.7.29.152>
2015-06-11 18:32:03 +0200

    Voeu

--- APLV1/src/pRectorat/Voeu.java
+++ APLV1/src/pRectorat/Voeu.java
@@ -7,47 +7,7 @@ package pRectorat;
 */
 public final class Voeu implements org.omg.CORBA.portable.IDLEntity
 {
-    public String getNoE() {
-		return noE;
-	}
-
-	public void setNoE(String noE) {
-		this.noE = noE;
-	}
-
-	public pRectorat.Accred getAcreditation() {
-		return acreditation;
-	}
-
-	public void setAcreditation(pRectorat.Accred acreditation) {
-		this.acreditation = acreditation;
-	}
-
-	public pRectorat.Rectorat getIdR() {
-		return idR;
-	}
-
-	public void setIdR(pRectorat.Rectorat idR) {
-		this.idR = idR;
-	}
-
-	public pRectorat.DecisionEtudiant getDecEtudiant() {
-		return decEtudiant;
-	}
-
-	public void setDecEtudiant(pRectorat.DecisionEtudiant decEtudiant) {
-		this.decEtudiant = decEtudiant;
-	}
-
-	public pRectorat.Etat getEtatVoeu() {
-		return etatVoeu;
-	}
-
-	public void setEtatVoeu(pRectorat.Etat etatVoeu) {
-		this.etatVoeu = etatVoeu;
-	}
-
-	/**
+    /**
      * Struct member noE
      */
     public String noE;
@@ -94,5 +54,77 @@ public final class Voeu implements org.omg.CORBA.portable.IDLEntity
         this.decEtudiant = decEtudiant;
         this.etatVoeu = etatVoeu;
     }
+
+	/**
+	 * @return the noE
+	 */
+	public String getNoE() {
+		return noE;
+	}
+
+	/**
+	 * @param noE the noE to set
+	 */
+	public void setNoE(String noE) {
+		this.noE = noE;
+	}
+
+	/**
+	 * @return the acreditation
+	 */
+	public pRectorat.Accred getAcreditation() {
+		return acreditation;
+	}
+
+	/**
+	 * @param acreditation the acreditation to set
+	 */
+	public void setAcreditation(pRectorat.Accred acreditation) {
+		this.acreditation = acreditation;
+	}
+
+	/**
+	 * @return the idR
+	 */
+	public pRectorat.Rectorat getIdR() {
+		return idR;
+	}
+
+	/**
+	 * @param idR the idR to set
+	 */
+	public void setIdR(pRectorat.Rectorat idR) {
+		this.idR = idR;
+	}
+
+	/**
+	 * @return the decEtudiant
+	 */
+	public pRectorat.DecisionEtudiant getDecEtudiant() {
+		return decEtudiant;
+	}
+
+	/**
+	 * @param decEtudiant the decEtudiant to set
+	 */
+	public void setDecEtudiant(pRectorat.DecisionEtudiant decEtudiant) {
+		this.decEtudiant = decEtudiant;
+	}
+
+	/**
+	 * @return the etatVoeu
+	 */
+	public pRectorat.Etat getEtatVoeu() {
+		return etatVoeu;
+	}
+
+	/**
+	 * @param etatVoeu the etatVoeu to set
+	 */
+	public void setEtatVoeu(pRectorat.Etat etatVoeu) {
+		this.etatVoeu = etatVoeu;
+	}
+    
+    
 
 }
