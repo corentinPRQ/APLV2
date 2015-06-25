13df435c06474ef11ea50d5934f3ef8cfc808e0f
guilhem <guilhem@192.168.1.40>
2015-06-25 14:30:35 +0200

    probleme verif du voeu corrigé

--- APLV1/src/GUI/IHM_FaireUnVoeu.java
+++ APLV1/src/GUI/IHM_FaireUnVoeu.java
@@ -266,17 +266,16 @@ public class IHM_FaireUnVoeu extends javax.swing.JFrame {
     	
     	Voeu voeu = new Voeu(parent.utilisateur.noEtu, parent.utilisateur.formation ,accredVoeu, new Rectorat(idRectorat), new Rectorat(nomRectDest),DecisionEtudiant.cree,Etat.cree);
     	//Avant de faire le voeu on va vérifier qu'il n'existe pas déja
-    	while(i<parent.listeVoeux.length && trouve!=true)
-    	{
-    		if(parent.listeVoeux[i].acredFormation.libelleD.equals(voeu.acredFormation.libelleD) 
-    				&& parent.listeVoeux[i].acredFormation.libelleU.equals(voeu.acredFormation.libelleU)){
+    	while(i<IHM_Etudiant.listeVoeux.length && trouve!=true)
+    	{    		if(IHM_Etudiant.listeVoeux[i].acredVoeu.libelleD.equals(voeu.acredVoeu.libelleD) 
+    				&& IHM_Etudiant.listeVoeux[i].acredVoeu.libelleU.equals(voeu.acredVoeu.libelleU)){
     			trouve=true;
     		}
     		i++;
     	}
     	
     	//Dans le cas ou le voeu n'existe pas le voeu va être enregistré
-    	if(trouve!=true){
+    	if(trouve){
     		clientEtuGV.faireVoeu(voeu);
          	
         	//On ne renvoi pas les voeux des autres académie là...
